2aea8ca765c9792de1336a91d8130bd9670a9fbe
NicolasMansoure <65515551+NicolasMansoure@users.noreply.github.com>
2023-02-20 11:33:08 +0100

    first

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+Cypress.on('uncaught:exception', () => {
+    return false;
+});
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+    it('Connection a un compte', () => {
+        cy.visit('https://www.doctolib.fr/')
+        cy.wait(500)
+        cy.get('#didomi-notice-disagree-button').click();
+
+
+    })
 })
